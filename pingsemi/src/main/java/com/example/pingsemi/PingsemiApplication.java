708096bef340746893243b51d69bd78b6425d9f2
coderromi <coderromi@gmail.com>
2025-08-04 22:48:43 +0900

    first setting

---
+++ pingsemi/src/main/java/com/example/pingsemi/PingsemiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pingsemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingsemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingsemiApplication.class, args);
+	}
+
+}
